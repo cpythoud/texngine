1ddea991010b1c9c1081c19503e8302fa3dfd4b3
cpythoud <git@pythoud.net>
2017-07-28 16:43:21 -0700

    Add footnote formatter helper class

---
+++ src/org/texngine/formatters/FootnoteFormatter.java
@@ -0,0 +1,8 @@
+package org.texngine.formatters;
+
+public class FootnoteFormatter {
+
+    public static String getSimpleFootnote(final String text) {
+        return "\\,\\footnote{" + text + "}";
+    }
+}
